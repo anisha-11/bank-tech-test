56ea70f2d55dbd19d27a49bf695dbca10c47997e
Anisha Hirani <anisha.hirani11@gmail.com>
2023-01-23 12:19:45 +0000

    2nd test failed - doing tdd

--- bank.test.js
+++ bank.test.js
@@ -4,3 +4,10 @@ it("returns inital empty statement", () => {
   const bank = new Bank();
   expect(bank.statement()).toBe("date || credit || debit || balance");
 });
+
+it("tracks 1st transaction of 1000 on 10/1/23", () => {
+  const bank = new Bank();
+  const deposit = new Transaction("credit", 1000, "10/1/23");
+  bank.add(deposit);
+  expect(bank.statement().toBe("date || credit || debit || balance\n10/1/23 || 1000.00 || || 1000.00"));
+});
